27935fb92e812c29ffc6b7d5c12efbf7cf103d1e
dang0x5f <dannypgehrz@gmail.com>
2025-01-30 19:49:10 -0500

    combined iphone parser

--- libutil.sh
+++ libutil.sh
@@ -1,3 +1,78 @@
+iphone_heic()
+{
+    DIR="/mnt/iPhone_Images/*"
+    ROOT="/mnt/iPhone_Images/"
+    for d in $DIR; do
+        if [ -d $d ]; then
+            for f in $d/*; do
+                if [ $(echo $f | cut -d. -f2) == "HEIC" ]; then
+                    mv $f ${ROOT}heicDirectory
+                fi
+            done
+        fi
+    done
+}
+
+iphone_png()
+{
+    DIR="/mnt/iPhone_Images/*"
+    ROOT="/mnt/iPhone_Images/"
+    for d in $DIR; do
+        if [ -d $d ]; then
+            for f in $d/*; do
+                if [ $(echo $f | cut -d. -f2) == "PNG" ]; then
+                    mv $f ${ROOT}pngDirectory
+                fi
+            done
+        fi
+    done
+}
+
+iphone_jpg()
+{
+    DIR="/mnt/iPhone_Images/*"
+    ROOT="/mnt/iPhone_Images/"
+    for d in $DIR; do
+        if [ -d $d ]; then
+            for f in $d/*; do
+                if [ $(echo $f | cut -d. -f2) == "JPG" ]; then
+                    mv $f ${ROOT}jpgDirectory
+                fi
+            done
+        fi
+    done
+}
+
+iphone_mov()
+{
+    DIR="/mnt/iPhone_Images/*"
+    ROOT="/mnt/iPhone_Images/"
+    for d in $DIR; do
+        if [ -d $d ]; then
+            for f in $d/*; do
+                if [ $(echo $f | cut -d. -f2) == "MOV" ]; then
+                    mv $f ${ROOT}movDirectory
+                fi
+            done
+        fi
+    done
+}
+
+iphone_aae()
+{
+    DIR="/mnt/iPhone_Images/*"
+    ROOT="/mnt/iPhone_Images/"
+    for d in $DIR; do
+        if [ -d $d ]; then
+            for f in $d/*; do
+                if [ $(echo $f | cut -d. -f2) == "AAE" ]; then
+                    mv $f ${ROOT}aaeDirectory
+                fi
+            done
+        fi
+    done
+}
+
 rm_spaces()
 {
     count=0
